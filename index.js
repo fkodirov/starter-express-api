0d40d8c43c67d76f193ce867dbece085e17a4a85
Farkhod Kodirov <farxod94@gmail.com>
2023-08-12 20:17:58 +0500

    Update

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const cors = require("cors");
+const cookieParse = require("cookie-parser");
+const router = require("./router/index");
+const errorMiddleware = require("./middleware/errorMiddleware");
+const PORT = 5000;
+
+const app = express();
+app.use(express.json());
+app.use(cookieParse());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
+app.use("/api", router);
+app.use(errorMiddleware);
+
+const start = () => {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+};
+
+start();
