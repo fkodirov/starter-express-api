5b2f91166e3358234f5fa3a1b71de82d72abfa18
Farkhod Kodirov <farxod94@gmail.com>
2023-08-13 01:45:46 +0500

    Upadate

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const cors = require("cors");
 const cookieParse = require("cookie-parser");
 const router = require("./router/index");
 const errorMiddleware = require("./middleware/errorMiddleware");
@@ -8,12 +7,6 @@ const PORT = 5000;
 const app = express();
 app.use(express.json());
 app.use(cookieParse());
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL,
-  })
-);
 app.use("/api", router);
 app.use(errorMiddleware);
 
