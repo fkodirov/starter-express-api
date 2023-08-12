4e46552cb3f6773681917bf82b8d40621e6cdfac
Farkhod Kodirov <farxod94@gmail.com>
2023-08-13 02:12:04 +0500

    Update

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const PORT = 5000;
 const app = express();
 app.use(express.json());
 app.use(cookieParse());
-app.options("*", cors());
+const allowedOrigins = process.env.REACT_APP_ALLOWED_ORIGINS.split(",");
+app.use(
+  cors({
+    credentials: true,
+    origin: allowedOrigins,
+  })
+);
 app.use("/api", router);
 app.use(errorMiddleware);
 
