322de05ed47d0f87111c6d9008d21bdc094977ae
Farkhod Kodirov <farxod94@gmail.com>
2023-08-13 09:23:23 +0500

    Update

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -27,6 +27,8 @@ class UserController {
       res.cookie("refreshToken", userData.refreshToken, {
         maxAge: 20 * 24 * 60 * 60 * 1000,
         httpOnly: true,
+        sameSite: "none",
+        secure: true,
       });
       return res.status(200).json({ ...userData });
     } catch (error) {
@@ -45,7 +47,7 @@ class UserController {
   }
   async refresh(req, res, next) {
     try {
-      const { refreshToken } = req.cookies;
+      const refreshToken = req.cookies.refreshToken;
       const userData = await userService.refresh(refreshToken);
       res.cookie("refreshToken", userData.refreshToken, {
         maxAge: 20 * 24 * 60 * 60 * 1000,
